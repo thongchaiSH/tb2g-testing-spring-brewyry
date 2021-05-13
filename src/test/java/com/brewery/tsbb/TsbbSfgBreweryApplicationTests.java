b0ad324497b7690771d7aabd7ecce80c1f36427e
thongchai <thongchai.sh@gmail.com>
2021-05-13 21:12:08 +0700

    initial

---
+++ src/test/java/com/brewery/tsbb/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.tsbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbbSfgBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
